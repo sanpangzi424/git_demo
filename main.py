d807c188acf6b47c90c27d6086a73b5a5e1be9fa
sanpangzi <18211100201@163.com>
2023-01-03 18:13:37 +0800

    第n+4次提交

--- main.py
+++ main.py
@@ -13,6 +13,6 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
     print("hello world1")
-    print("hello bug1")
+    print("hello bug12")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
