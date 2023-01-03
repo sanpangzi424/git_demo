1c4d5fba4e69afe1405b1aad56a9e92791cd9171
sanpangzi <18211100201@163.com>
2023-01-03 10:49:00 +0800

    dev 111

--- main.py
+++ main.py
@@ -13,6 +13,6 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
     print("hello world3")
-    print("hello world")
+    print("hello dev")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
